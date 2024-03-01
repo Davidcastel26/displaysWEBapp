8862dbff9ce7387e252801e1c280d9d1727fa898
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-03-01 10:07:48 -0600

    Update Location.tsx

--- app/(orders)/order/[components]/Location.tsx
+++ app/(orders)/order/[components]/Location.tsx
@@ -1,15 +1,30 @@
-import { MapPin } from 'lucide-react';
+
+import { MapPin,
+        ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
 export const Location = () => {
+
+    const supermarketAddress = `Supermercado La Torre C.C. Flores Del Lago`
+
   return (
     <div className='flex flex-row p-4'>
-        <div className='align-bottom mt-3 mr-4'>
+        <div className='pt-[2rem] mr-3'>
             <MapPin />
         </div>
+        <Link href="/stores" >
         <div className='flex-col'>
-            <h4>Tu tienda selecionada para el pedido es:</h4>
-            <p> La torre X</p>
+            <h4 className='scroll-m-10 text-lg font-semibold tracking-tight first:mt-0'>
+                Tu tienda selecionada para el pedido es:
+            </h4>
+            <div className='flex m-1'>
+                <p className="scroll-m-10 border-b pb-2 text-lg font-light tracking-tight first:mt-0">
+                    { supermarketAddress }
+                </p>
+                <ArrowRight />
+            </div>
         </div>
+        </Link>
     </div>
   )
 }
