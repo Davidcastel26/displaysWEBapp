8cb23e238aa498db44256381f0595a5862dbca53
Davidcastel26 <davcastellanoslarios@gmail.com>
2024-03-01 14:51:46 -0600

    Update itemsReviewed.tsx

--- components/orders/itemsReviewed.tsx
+++ components/orders/itemsReviewed.tsx
@@ -10,13 +10,19 @@ export const ItemsReviewed = () => {
             img: 'https://latorremx.vtexassets.com/arquivos/ids/165752-1600-auto?v=637685360512630000&width=1600&height=auto&aspect=true',
             amount: 3,
         },
+        {
+            id: nanoid(),
+            productTitleOrdered :"Papaya",
+            img: 'https://latorremx.vtexassets.com/arquivos/ids/165752-1600-auto?v=637685360512630000&width=1600&height=auto&aspect=true',
+            amount: 3,
+        },
     ]
 
   return (
-    <div className="pb-[2rem] mt-[1rem] pt-[1rem] divide-y divide-y-reverse flex flex-col">
+    <div className="pb-[2rem] mt-[1rem] pt-[1rem] flex flex-col">
         {
             itemsPlaced.map((e) => (
-               <div key={ e.id }>
+               <div key={ e.id } className="divide-y divide-y-reverse ">
                 <img src={e.img} alt="product" />
                 <div>
                     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
